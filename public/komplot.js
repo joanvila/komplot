e7c21b98c04febeab860f0fc72326e974b8e98e3
Ricard Gascons <gascons1995@gmail.com>
2016-04-17 07:50:48 +0200

    All coins

--- public/komplot.js
+++ public/komplot.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(1152+64, 448+64, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update:update, render:render });
+var game = new Phaser.Game(1216, 512, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update:update, render:render });
 
 function preload() {
 
@@ -13,9 +13,24 @@ var map;
 var layer;
 var player;
 var cursors;
-var facing = 'left';
+var facing = 'right';
 var jumpTimer = 0;
 var coinGroup;
+
+var coins = [
+    [4,2],[5,2],[6,2],[7,2],
+    [2,5],[3,5],[4,5],[7,5],[8,5],
+    [3,8],[5,8],[7,8],[9,8],
+    [4,11],[5,11],[6,11],[7,11],[8,11],
+    [11,4],[12,4],[14,3],[15,3],[17,1],[18,1],[17,2],[18,2],
+    [12,12],[14,11],[16,10],[18,6],[18,7],[18,8],[20,10],[22,11],[24,12],
+    [22,4],[25,4],[23,3],[24,3],[23,2],[24,2],[22,1],[25,1],
+    [34,7],[35,7],[28,11],[29,11],[31,9],[32,9],[30,2],[31,1],[32,1],[33,2],[28,7],[29,7],[28,11],[29,11],[34,11],[35,11],
+    [15,13],[17,12],[19,12],[21,13],
+
+];
+var TILE_X = 38;
+var TILE_Y = 16;
 
 function create() {
 
@@ -32,14 +47,8 @@ function create() {
     //  The second parameter maps this name to the Phaser.Cache key 'tiles'
     map.addTilesetImage('tiles', 'tiles');
     
-
-
-
-    //map.setCollisionBetween(15, 16);
-    //map.setCollisionBetween(20, 25);
-    //map.setCollisionBetween(27, 29);
     for (var i = 0; i < 61; ++i) {
-        if (i !== 27) {
+        if (/*i !== 28 && */i !== 27) {
             map.setCollision(i);
         }
     }
@@ -48,15 +57,17 @@ function create() {
     //  A Layer is effectively like a Phaser.player, so is added to the display list.
     layer = map.createLayer('Tile Layer 1');
 
-    //layer.debug = true;
+    layer.debug = true;
 
     //  This resizes the game world to match the layer dimensions
     layer.resizeWorld();
 
     coinGroup = game.add.group();   
-    coinGroup.enableBody = true;     
-    coinGroup.create(100, 100, 'coin');
-    //map.createFromObjects('Tile Layer 1', 50, 'coin', 0, true, false, coinGroup);      
+    coinGroup.enableBody = true;
+    coins.forEach(function(c) {
+        coin = coinGroup.create((1216/TILE_X)*c[0], (512/TILE_Y)*c[1], 'coin');
+        coin.body.allowGravity = false;
+    })
     coinGroup.callAll('animations.add', 'animations', 'spin', [0, 1, 2, 3, 4, 5], 10, true);  
     coinGroup.callAll('animations.play', 'animations', 'spin');
 
@@ -65,6 +76,8 @@ function create() {
     player.body.bounce.y = 0.2;
     player.body.collideWorldBounds = true;
     player.body.setSize(16, 16, 8, 8);
+    player.anchor.x = 0.5;
+    player.anchor.y = 0.5;
 
 
     game.camera.follow(player);
@@ -100,20 +113,20 @@ function update() {
     {
         player.body.velocity.x = -125;
 
-        /*if (facing != 'left')
+        if (facing != 'left')
         {
-            player.animations.play('left');
+            player.angle -= -180;
             facing = 'left';
-        }*/
+        }
     } else if (cursors.right.isDown)
     {
         player.body.velocity.x = 125;
 
-        /*if (facing != 'right')
+        if (facing != 'right')
         {
-            player.animations.play('right');
+            player.angle += 180;
             facing = 'right';
-        }*/
+        }
     }
     
     if (jumpButton.isDown && player.body.onFloor())
@@ -131,10 +144,11 @@ function update() {
 
 function render () {
 
-    game.debug.text(game.time.suggestedFps, 32, 32);
+    //game.debug.text(game.time.suggestedFps, 32, 32);
 
     // game.debug.text(game.time.physicsElapsed, 32, 32);
     game.debug.body(player);
+    game.debug.body(map);
     // game.debug.bodyInfo(player, 16, 24);
 
 }
